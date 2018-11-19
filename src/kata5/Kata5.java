2e671902e98d1e02a98f72863235e1c1ad092f28
Yousuf Boutahar El Maachi <yusef_boutahar@hotmail.com>
2018-11-19 16:41:02 +0000

    commit nº1

---
+++ src/kata5/Kata5.java
@@ -0,0 +1,99 @@
+package kata5;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Kata5 {
+	
+	 // Cadena de conexión SQLite
+	static String url = "jdbc:sqlite:KATA5.db";
+	
+	public static void main(String[] args) {
+		createNewDatabase("KATA5.db");
+		createNewTable();
+		
+		Kata5 idt = new Kata5();
+		// Se insertar 3 nuevas líneas
+		idt.insert("Yusra","Boutahar","DIS");
+		idt.insert("Pedro","Almodobar","PSOE");
+		idt.insert("Hasan","Boutahar","GUI");
+		
+		idt.selectAll();
+	}
+	
+	public static void createNewDatabase(String filename) {
+		String url = "jdbc:sqlite:" + filename ;
+		
+		try (Connection conn = DriverManager.getConnection(url)) {
+			if (conn != null) {
+				DatabaseMetaData meta = conn.getMetaData();
+				System.out.println("El driver es " + meta.getDriverName());
+				System.out.println("Se ha creado una nueva BD.");
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		} 
+	}
+	
+	public static void createNewTable() {
+
+		// Instrucción SQL para crear nueva tabla
+		String sql = "CREATE TABLE IF NOT EXISTS PEOPLE (\n"
+						+ " id integer PRIMARY KEY AUTOINCREMENT,\n"
+							+ " Name text NOT NULL, \n"
+								+ "Apellidos text NOT NULL, \n"
+									+ "Departamento text NOT NULL);";
+		
+		try (Connection conn = DriverManager.getConnection(url);
+				Statement stmt = conn.createStatement()) {
+			// Se crea la nueva tabla
+			stmt.execute(sql);
+			System.out.println("Tabla creada");
+		
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	// Método para insertar datos en la tabla direcc_email
+	public void insert(String Name, String Apellidos, String Dept) {
+		String sql = "INSERT INTO PEOPLE(Name,Apellidos,Departamento) VALUES(?,?,?)";
+		
+		try (Connection conn = DriverManager.getConnection(url);
+		PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			
+			pstmt.setString(1, Name);
+			pstmt.setString(2, Apellidos);
+			pstmt.setString(3, Dept);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void selectAll() {
+		String sql = "SELECT * FROM PEOPLE";
+		
+		try (Connection conn = DriverManager.getConnection(url);
+			 Statement stmt = conn.createStatement();
+			 ResultSet rs = stmt.executeQuery(sql)){
+			
+			// Bucle sobre el conjunto de resgistros.
+			while(rs.next()) {
+				System.out.println( rs.getInt("id") + "\t" +
+									rs.getString("Name") + "\t" +
+									rs.getString("Apellidos") + "\t" +
+									rs.getString("Departamento") + "\t");
+			}
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
